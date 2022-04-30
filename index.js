7d108b292aad9516e0b10334d590a88968108dfe
Nakshatra bansal <75303956+naksh2004@users.noreply.github.com>
2022-04-30 16:30:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ document.getElementById('file').addEventListener("change", function (e) {
     var data = f.target.result;                    
     window.fabric.Image.fromURL(data, function (img) {
       img.set({
-        left: 400,
-        top: 100
+        left: 300,
+        top: 0
       });
-      img.scaleToHeight(500);
-      img.scaleToWidth(500);
+      img.scaleToHeight(300);
+      img.scaleToWidth(300);
       canvas.add(img);
       var a = canvas.setActiveObject(oImg);
       var dataURL = canvas.toDataURL({format: 'jpg', quality: 1});
