9d66aee3ab932ff1e6a8ed4bb463b381d846541b
Nakshatra bansal <75303956+naksh2004@users.noreply.github.com>
2022-04-30 16:22:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ document.getElementById('file').addEventListener("change", function (e) {
   var reader = new FileReader();
   reader.onload = function (f) {
     var data = f.target.result;                    
-    fabric.Image.fromURL(data, function (img) {
-      var oImg = img.set({left: 400, top: 100, angle: 0,width:1500, height:600}).scale(0.9);
-      canvas.add(oImg).renderAll();
+    window.fabric.Image.fromURL(data, function (img) {
+      img.set({
+        left: 400,
+        top: 100
+      });
+      img.scaleToHeight(500);
+      img.scaleToWidth(500);
+      canvas.add(img);
       var a = canvas.setActiveObject(oImg);
-      var dataURL = canvas.toDataURL({format: 'jpg', quality: 0.8});
-    });
+      var dataURL = canvas.toDataURL({format: 'jpg', quality: 1});
+    })
   };
   reader.readAsDataURL(file);
 });
