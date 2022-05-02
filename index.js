40f7d6299bf0ae7a6ae136f0adcc1d191ed224e6
Nakshatra bansal <75303956+naksh2004@users.noreply.github.com>
2022-05-02 14:17:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,17 +15,31 @@ document.getElementById('file').addEventListener("change", function (e) {
       var a = canvas.setActiveObject(oImg);
       var dataURL = canvas.toDataURL({format: 'jpg', quality: 1});
     })
-    canvas.on('mouse:wheel', function(opt) {
-  var delta = opt.e.deltaY;
-  var pointer = canvas.getPointer(opt.e);
-  var zoom = canvas.getZoom();
-  zoom = zoom + delta/200;
-  if (zoom > 20) zoom = 20;
-  if (zoom < 0.01) zoom = 0.01;
-  canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
-  opt.e.preventDefault();
-  opt.e.stopPropagation();
-});
+    canvas.on('mouse:wheel', function(img) {
+      var delta = img.e.deltaY;
+      var zoom = canvas.getZoom();
+      zoom *= 0.999 ** delta;
+      if (zoom > 20) zoom = 20;
+      if (zoom < 1) zoom = 1;
+      canvas.zoomToPoint({ x: img.e.offsetX, y: img.e.offsetY }, zoom);
+      img.e.preventDefault();
+      img.e.stopPropagation();
+      var vpt = canvas.viewportTransform;
+      if (zoom < 400 / 1000) {
+        vpt[4] = 200 - 1500 * zoom / 2;
+        vpt[5] = 200 - 600 * zoom / 2;
+      } else {
+        if (vpt[4] >= 0) {
+          vpt[4] = 0;
+        } else if (vpt[4] < canvas.getWidth() - 1500 * zoom) {
+          vpt[4] = canvas.getWidth() - 1500 * zoom;
+        }
+        if (vpt[5] >= 0) {
+          vpt[5] = 0;
+        } else if (vpt[5] < canvas.getHeight() - 600 * zoom) {
+          vpt[5] = canvas.getHeight() - 600 * zoom;
+        }
+      }})
   };
   reader.readAsDataURL(file);
 });
